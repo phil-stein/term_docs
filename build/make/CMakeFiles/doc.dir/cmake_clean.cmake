66eacbcd8f6d40801e100a3930fa7a0dc57d2a16
Phil Desktop <philipp033.25@gmail.com>
2024-03-07 02:21:52 +0100

    DESKTOP: utf8 support, working on nerdfont icon support

---
+++ build/make/CMakeFiles/doc.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "C:/Workspace/C/term_docs/bin/doc.exe"
+  "C:/Workspace/C/term_docs/bin/doc.exe.manifest"
+  "C:/Workspace/C/term_docs/bin/doc.pdb"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/config.c.obj"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/config.c.obj.d"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/core_data.c.obj"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/core_data.c.obj.d"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/def.c.obj"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/def.c.obj.d"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/doc.c.obj"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/doc.c.obj.d"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/file_io.c.obj"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/file_io.c.obj.d"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/main.c.obj"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/main.c.obj.d"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/style.c.obj"
+  "CMakeFiles/doc.dir/C_/Workspace/C/term_docs/src/app/style.c.obj.d"
+  "libdoc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
