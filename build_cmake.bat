df430ada97bedecb9d39c7a3e000feb71594833c
Phil-Laptop <philipp033.25@gmail.com>
2025-06-12 16:41:56 +0200

    LAPTOP: sync with desktop

--- build_cmake.bat
+++ build_cmake.bat
@@ -8,9 +8,10 @@ cmake -G "MinGW Makefiles" . -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 :: copy compile_commands.json to root for clangd lsp
 copy /y compile_commands.json ..\..\compile_commands.json
 
-:: --- build vs19 ---
-copy /y CMakeLists.txt ../vs19/CMakeLists.txt
-cd ..\vs22
-cmake -S..\make -B . -G "Visual Studio 17 2022"  
+:: commented cause laptop still has vs19
+:: :: --- build vs19 ---
+:: copy /y CMakeLists.txt ../vs19/CMakeLists.txt
+:: cd ..\vs22
+:: cmake -S..\make -B . -G "Visual Studio 17 2022"  
 
 cd ../..
